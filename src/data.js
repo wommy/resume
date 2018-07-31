3a294d9ce8f6024963923d0ea8e059c1b5f5f8da
wommy <tom@technomad.media>
2018-07-31 17:14:51 -0400

    cleaned old, streamlining data

---
+++ src/data.js
@@ -0,0 +1,35 @@
+contact
+  picture
+  tommy williams
+  email - 9@w0m.io
+  phone - 312 818 9011
+  portfolio - wommy.io, github.com/wommy
+  focus - digital publishing, media distribution =&gt; eCommerce
+  location - chicago =&gt; royal oak mi =&gt; berlin
+  hobbies - cycling martial-arts dancing techno
+  built with - chromeos cloud9.io github node pugjs scss
+
+education
+  depaul university - class of 2015
+    bachelors of art - psychology
+    minor - interactive and social media
+  columbia college chicago
+
+experience
+  since 2010 - technomad - freelance
+  since 2015 - solution people
+  2015 summer/fall  - original graphics printing
+
+skills
+  node, javascript, es6
+  git, github, git flow
+  pugJS, html5, json
+  scss, css3, sass, less
+  linux, cli, chromeos
+  static site generators
+  build tools, npm, webpack
+  component-based architecture
+  cdn, dns, ssl, https/2
+  object-oriented, functional
+  ruby python react vue riot
+  ui ux, gimp, sketch
---
+++ src/data1.js
@@ -0,0 +1,34 @@
+contact
+  picture
+  tommy williams
+  email - tom@technomad.media
+  portfolio - github.com/wommy, wommy.io, technomad.media
+  focus - digital publishing, media distribution, ecommerce
+  location - chicago,il royal oak,mi newyork,ny
+  hobbies - cycling martial arts dancing techno
+  built with - linux github atom javascript html css
+
+education
+  depaul university - class of 2015
+    bachelors of art - psychology
+    minor - interactive and social media
+  columbia college chicago
+
+experience
+  since 2010 - technomad - freelance
+  since 2015 - solution people
+  2015 summer/fall  - original graphics printing
+
+skills
+  node, javascript, es6
+  git, github, git flow
+  pugJS, html5, json
+  scss, css3, sass, less
+  linux, cli, chromeos
+  static site generators
+  build tools, npm, webpack
+  component-based architecture
+  cdn, dns, ssl, https/2
+  object-oriented, functional
+  ruby python react vue
+  ui ux, gimp, sketch
