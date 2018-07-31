93c1fcbbb6a1513a5efcbc0fdedbdb6ce4c14a57
wommy <tom@technomad.media>
2018-07-31 19:03:32 -0400

    added conferences

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ contact
   email: tom@technomad.media
   portfolio: github.com/wommy, wommy.io, technomad.media
   focus: digital publishing, media distribution, ecommerce
-  location: chicago,il royal oak,mi newyork,ny
+  location: chicago,il; royal oak,mi; newyork,ny
   hobbies: cycling, martial arts, dancing, techno
   built with: linux, github, atom, javascript, html, css
   view online: github.com/wommy/resume
@@ -17,12 +17,48 @@ education
     minor - interactive and social media
 
 experience
-  since 2010: web freelancer:
-    24+ microbusinesses; development, design, consultation
+  since 2010: technomad:
+    web freelancer: 24+ microbusinesses; development, design, consultation
   2015 - 2017: solution people:
     lead design, development, content, database; 30+ websites
   2015 summer, fall: original graphics printing:
     subcontractor; 5+ brands: website, signage, decals, tshirts
+
+conferences / meetups
+  2017/04/06 - theNewDynamic @ Carrot Creative, NY,NY
+    cms as code, pushing the limits w/ static site tools
+  2017/04/27 - Active Ingredients @ Google, SF,CA
+    activeingredients.info by Netlify
+  2017/10/02 - JAMstack NYC @ O’Reilly’s Velocity, NY,NY
+    Git-based or API-driven CMS - by Chris Macrae of forestry.io
+  2017/10/11 - Elm Philadelphia @ Blackfynn, Philly,PA
+    Beginners Night! - by Richard Feldman
+  2017/11/06 - Detroit Javascript @ Detroit Labs, DET,MI
+    Lets Kill CSS: Pros and Cons of CSS in JS - by Steve Barman and Dennay Bedard
+  2018/11/13 - SE Michigan JavaScript @ Barracuda Networks, Ann Arbor,MI
+    Exploring Vue.js, an alternative to Angular and React - by Jacob Bolton
+  2018/01/31 - Detroit Javascript @ Detroit Labs, DET,MI
+    Mob Coding! Build a server-side NextJS blog
+  2018/02/22 - Detroit Javascript @ Detroit Labs, DET,MI
+    Programming with Attention Deficit Disorder - by Steve Barman
+  2018/03/21 - Detroit Javascript @ OtterBase, Livonia,MI
+    Group coding with exercism.io - by Mike Matola
+  2018/03/27 - HackerNest Detroit @ i3Detroit, Ferndale,MI
+    March Social
+  2018/04/09 - SE Michigan JavaScript @ Barrcuda Networks, Ann Arbor,MI
+    ASTronomically Improving your Developer Experience: Abstract Syntax Tree Primer - by Bernardo Pacheco of Dominos Pizza
+  2018/04/24 - New York Web Performance Group @ Spotify, NY,NY
+    Building a Static HTML & Web App Frankenstein - by Jeff Escalante of Hashicorp
+  2018/05/01 - Detroit Javascript @ RouteOne, Farmington Hills,MI
+    Securing VueJS Applications with JSOn Web Tokens - by Ado Kukic of Auth0
+  2018/06/18 - Detroit Javascript @ Detroit Labs, DET,MI
+    a FUNctional Javascript Makeover - by Mike Schutte
+  2018/07/10 - New York Web Performance Group @ Priceline, NY,NY
+    Meet for SPEED: Lets talk Webpack - by Sergey Chernyshev of Meetup.com and Malek Hakim of Priceline.com
+  2018/07/18 - New York Web Performance Group @ Squarespace, NY,NY
+    FaCSSt: CSS & Performance - by Harry Roberts, csswizardy.com
+
+  member of: detroit.js, sem.js, detroit react developers
 
 skills
   hypertext markup language, cascading style sheets, javascript
